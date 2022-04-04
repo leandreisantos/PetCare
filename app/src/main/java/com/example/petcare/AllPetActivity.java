7e7d72deacfe2f65105df6fd371cbf0a529b03ff
Leandrei santos <leandreisantos10@gmail.com>
2022-04-04 10:34:15 +0800

    first-commit

---
+++ app/src/main/java/com/example/petcare/AllPetActivity.java
@@ -0,0 +1,14 @@
+package com.example.petcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllPetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_pet);
+    }
+}
--- app/src/main/java/com/example/petcare/ProfileFragment.java
+++ app/src/main/java/com/example/petcare/ProfileFragment.java
@@ -1,13 +1,19 @@
 package com.example.petcare;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +23,12 @@ public class ProfileFragment extends Fragment {
 
     TextView logoutholder;
     FirebaseAuth mAuth;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String currentid = user.getUid();
+
+    ImageView ivholder;
+    TextView namholder,contactholder,addressholder,emailholder;
+    ImageView pet,addpet;
 
     @Nullable
     @Override
@@ -29,15 +41,76 @@ public class ProfileFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         logoutholder = getActivity().findViewById(R.id.tv_logout_pf);
+        namholder = getActivity().findViewById(R.id.tv_name_p);
+        ivholder = getActivity().findViewById(R.id.iv_pic);
+        contactholder = getActivity().findViewById(R.id.tv_contact_p);
+        addressholder = getActivity().findViewById(R.id.tv_address_p);
+        emailholder = getActivity().findViewById(R.id.tv_email_p);
+        pet = getActivity().findViewById(R.id.tv_pet);
+        addpet = getActivity().findViewById(R.id.tv_addpet);
+
         mAuth = FirebaseAuth.getInstance();
 
 
-        logoutholder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAuth.signOut();
-                startActivity(new Intent(getActivity(),LoginActivity.class));
-            }
+        logoutholder.setOnClickListener(v -> showlogout());
+
+        pet.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(),AllPetActivity.class);
+            startActivity(intent);
         });
+        addpet.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(),CreateNewPetActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void showlogout() {
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View view = inflater.inflate(R.layout.logout_layout,null);
+        TextView logout_tv = view.findViewById(R.id.logout_tv_ll);
+        TextView cancel_tv = view.findViewById(R.id.cancel_tv_ll);
+
+        AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+                .setView(view)
+                .create();
+        alertDialog.show();
+        logout_tv.setOnClickListener(v -> {
+            mAuth.signOut();
+            startActivity(new Intent(getActivity(),LoginActivity.class));
+        });
+        cancel_tv.setOnClickListener(v -> alertDialog.dismiss());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        DocumentReference reference;
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        reference = firestore.collection("user").document(currentid);
+
+        reference.get()
+                .addOnCompleteListener(task -> {
+                    if(task.getResult().exists()){
+
+                        String nameResult = task.getResult().getString("name");
+                        String contacResult = task.getResult().getString("mobile");
+                        String addreesResult = task.getResult().getString("add");
+                        String urlResult = task.getResult().getString("url");
+                        String emailResult = task.getResult().getString("email");
+
+                        Picasso.get().load(urlResult).into(ivholder);
+                        namholder.setText(nameResult);
+                        contactholder.setText(contacResult);
+                        addressholder.setText(addreesResult);
+                        emailholder.setText(emailResult);
+
+                    }else{
+                        Intent intent = new Intent(getActivity(),CreateProfileActivity.class);
+                        startActivity(intent);
+                    }
+                });
+
     }
 }
