43e397debbf88642f92a126c0d57655bc3aa1631
Leandrei santos <leandreisantos10@gmail.com>
2022-04-16 15:06:44 +0800

    first-commit

--- app/src/main/java/com/example/petcare/OptionSignUpActivity.java
+++ app/src/main/java/com/example/petcare/OptionSignUpActivity.java
@@ -21,10 +21,94 @@ public class OptionSignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_option_sign_up);
 
         owner = findViewById(R.id.et_email_login);
+        bussiness = findViewById(R.id.et_pass_login);
+        back = findViewById(R.id.tv_back);
 
         owner.setOnClickListener(v -> showdialogowner());
+        bussiness.setOnClickListener(v -> showdialogBuss());
 
         back.setOnClickListener(v -> onBackPressed());
+    }
+
+    private void showdialogBuss() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.owner_signin_dialog1,null);
+
+        CardView next = view.findViewById(R.id.cv3);
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setView(view)
+                .create();
+        alertDialog.show();
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showdialog2();
+                alertDialog.dismiss();
+            }
+        });
+
+    }
+
+    private void showdialog2() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.owner_signin_dialog2,null);
+
+        CardView next = view.findViewById(R.id.cv3);
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setView(view)
+                .create();
+        alertDialog.show();
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showdialog3();
+                alertDialog.dismiss();
+            }
+        });
+    }
+
+    private void showdialog3() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.owner_signin_dialog3,null);
+
+        CardView next = view.findViewById(R.id.cv3);
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setView(view)
+                .create();
+        alertDialog.show();
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showdialog4();
+                alertDialog.dismiss();
+            }
+        });
+    }
+
+    private void showdialog4() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.owner_signin_dialog4,null);
+
+        CardView next = view.findViewById(R.id.cv3);
+
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setView(view)
+                .create();
+        alertDialog.show();
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showdialog4();
+                alertDialog.dismiss();
+            }
+        });
     }
 
     private void showdialogowner() {
