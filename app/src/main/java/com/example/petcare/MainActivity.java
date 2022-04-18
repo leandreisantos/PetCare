65dc67ccdd0709ed250c25d09040cfc75c3676d1
Leandrei santos <leandreisantos10@gmail.com>
2022-04-19 01:42:48 +0800

    first-commit

--- app/src/main/java/com/example/petcare/MainActivity.java
+++ app/src/main/java/com/example/petcare/MainActivity.java
@@ -52,16 +52,39 @@ public class MainActivity extends AppCompatActivity {
 //        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new homeFragment()).commit();
     }
     private BottomNavigationView.OnNavigationItemSelectedListener onNav = item -> {
-        Fragment selected = null;
-        switch(item.getItemId()){
-            case R.id.home_bottom:
-                selected = new homeFragment();
-                break;
-            case R.id.profile_bottom:
-                selected = new ProfileFragment();
-                break;
-        }
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,selected).commit();
+
+        DocumentReference reference;
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        reference = firestore.collection("user").document(currentuid);
+
+        reference.get()
+                .addOnCompleteListener(task -> {
+                    if(task.getResult().exists()){
+
+                        String statusResult = task.getResult().getString("status");
+                        Fragment selected = null;
+
+                        if(statusResult.equals("Bussiness")){
+                            switch(item.getItemId()){
+                                case R.id.home_bottom:
+                                    selected = new BusinessHomeFragment();
+                                    break;
+                                case R.id.profile_bottom:
+                                    selected = new ProfileFragment();
+                                    break;
+                                case R.id.calendar_bottom:
+                                    selected = new BusinessCalendarFragment();
+                                    break;
+                            }
+                            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,selected).commit();
+                        }
+
+                    }else{
+                        Intent intent = new Intent(MainActivity.this,CreateProfileActivity.class);
+                        startActivity(intent);
+                    }
+                });
         return true;
     };
 
